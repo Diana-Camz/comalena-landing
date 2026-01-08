0059f0e679cc398114b88bbe7b40e008a5bb49e1
Diana-Camz <dcampos0495@gmail.com>
2026-01-07 22:36:26 -0800

    feat: create menu page with menuCard and styles

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,5 +1,19 @@
+import Layout from "@/components/Layout";
+import MenuCard from "@/components/MenuCard";
+import { pizzasMenu } from "@/components/data/data";
+
 export default function menuPage() {
     return (
-        <div>menuPage</div>
+        <Layout>
+            <section className="py-8">
+                <h1 className="font-display lg:text-6xl text-center">Menú de Comaleña Pizza</h1>
+                <div className="lg:mt-12 grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
+                    {pizzasMenu.map((pizza, index) => (
+                        <MenuCard key={index} {...pizza}/>
+                    ))}
+                </div>
+
+            </section>
+        </Layout>
     )
 }
---
+++ components/MenuCard.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+type MenuCardProps = {
+  id: number;
+  title: string;
+  ingredients: string;
+  imageUrl: string;
+  prices: string[];
+  tags?: string[];
+}
+
+export default function MenuCard({id, title, ingredients, imageUrl, prices, tags}: MenuCardProps) {
+  return (
+    <div className="flex border justify-start p-3 items-center rounded-lg ">
+      <div className="">
+        <Image src={imageUrl} alt={title} width={100} height={100} className="rounded-lg object-cover h-36 w-44"/>
+      </div>
+      <div className="w-110">
+        <h3 className="font-display text-2xl ml-4">{title}</h3>
+        <p className="text-foreground/80 mt-2 ml-4">{ingredients}</p>
+        <div className="flex w-50 justify-between mt-4 ml-4">
+          {prices.map((price, index) => (
+            <span key={index} className="text-xl">{price}</span>
+          ))}
+        </div>
+      </div>
+    </div>
+  )}
