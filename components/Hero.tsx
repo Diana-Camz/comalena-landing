6b919085fc291acb3c562da8d9d85b199de42ae7
Diana-Camz <dcampos0495@gmail.com>
2026-02-01 18:24:11 -0800

    feat: add hero images and integrate HeroCarusel component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,15 @@
 'use client';
 import Image from "next/image";
+import { HeroCarousel } from "./HeroCarousel";
 
 export function Hero () {
     return (
-        <div className="w-full min-[600px]:w-[full] relative mt-8 aspect-25/9 shrink-0 overflow-hidden">
-            <Image src="/images/margarita.jpg" alt="Hero Image Comaleña Pizza" fill sizes="(max-width: 600px) 100vw, 600px"  className="rounded-4xl object-cover" priority/>
-        </div>
+        // <div className="w-full min-[600px]:w-full relative mt-8 
+        // aspect-square
+        // md:aspect-[16/9]     
+        // lg:aspect-[25/9] shrink-0 overflow-hidden">
+        //     <Image src="/images/margarita.jpg" alt="Hero Image Comaleña Pizza" fill sizes="(max-width: 600px) 100vw, 1000px"  className="rounded-4xl object-cover" priority/>
+        // </div>
+        <HeroCarousel />
     )
 }
