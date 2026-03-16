a2620fb10b1d2ef6139015c0fe674d7c519af008
Diana-Camz <dcampos0495@gmail.com>
2026-03-15 20:10:17 -0700

    feat: update prices

--- data/data.js
+++ data/data.js
@@ -1,114 +1,114 @@
 export const pizzasMenu = [
   {
     id: 1,
-    title: "Pepperoni",
-    ingredients: "Salsa de tomate, queso y pepperoni",
+    title: "Básica",
+    ingredients: "Salsa de tomate, queso y 1 ingrediente a elegir",
     imageUrl: "/images/menu/pepperoni.webp",
-    prices: {sm: 69, md: 139, lg: 179},
-    tags: ["carnes", "clasica", "mas-pedida"]
+    prices: {sm: 79, md: 159, lg: 219},
+    tags: ["basica", "mas-pedida"]
   },
   {
     id: 2,
     title: "Pepperoni Champiñones",
     ingredients: "Salsa de tomate, queso, pepperoni y champiñones",
     imageUrl: "/images/menu/pepperoni-champ.webp",
-    prices: {sm: 79, md: 159, lg: 199},
-    tags: ["mas-pedida", "carnes", "clasica"]
+    prices: {sm: 85, md: 169, lg: 239},
+    tags: ["mas-pedida", "carnes", "tradicional"]
   },
   {
     id: 3,
     title: "Hawaiana",
     ingredients: "Salsa de tomate, queso, jamón y piña",
     imageUrl: "/images/menu/hawaiana.webp",
-    prices: {sm: 79, md: 159, lg: 199},
-    tags: ["carnes", "clasica", "mas-pedida"]
+    prices: {sm: 85, md: 169, lg: 239},
+    tags: ["carnes", "tradicional", "mas-pedida"]
   },
   {
     id: 4,
     title: "Red Hawaiana",
     ingredients: "Salsa de tomate, queso, pepperoni, piña",
     imageUrl: "/images/menu/red-haw.webp",
-    prices: {sm: 79, md: 159, lg: 199},
-    tags: ["carnes", "clasica"]
+    prices: {sm: 85, md: 169, lg: 239},
+    tags: ["carnes", "tradicional"]
   },
   {
     id: 5,
     title: "Mexicana",
     ingredients: "Salsa de tomate, queso, chorizo, jitomate, pimiento, jalapeño y cebolla",
     imageUrl: "/images/menu/mexicana.webp",
-    prices: {sm: 79, md: 169, lg: 209},
-    tags: ["picante", "carnes", "clasica", "mas-pedida"]
+    prices: {sm: 85, md: 169, lg: 239},
+    tags: ["picante", "carnes", "tradicional", "mas-pedida"]
   },
   {
     id: 6,
     title: "Vegetariana",
     ingredients: "Salsa de tomate, queso, champiñones, pimiento, cebolla, jalapeño y piña",
     imageUrl: "/images/menu/vegetariana.webp",
-    prices: {sm: 79, md: 169, lg: 209},
-    tags: ["vegetariana", "picante"]
+    prices: {sm: 85, md: 169, lg: 239},
+    tags: ["vegetariana", "picante", 'tradicional']
   },
   {
     id: 7,
     title: "Margarita",
     ingredients: "Salsa de tomate, queso, jitomate cherry, albahaca y queso parmesano",
     imageUrl: "/images/menu/margarita.webp",
-    prices: {sm: 79, md: 179, lg: 219},
-    tags: ["vegetariana", "clasica"]
+    prices: {sm: 89, md: 189, lg: 259},
+    tags: ["vegetariana", "de-la-casa"]
   },
   {
     id: 8,
     title: "Mamma-Mía",
     ingredients: "Salsa de tomate, queso, jitomate deshidratado, jitomate cherry, champiñones y albahaca fresca",
     imageUrl: "/images/menu/mamma-mia.webp",
-    prices: {sm: 79, md: 189, lg: 229},
-    tags: ["vegetariana", "especial"]
+    prices: {sm: 89, md: 189, lg: 259},
+    tags: ["vegetariana", "de-la-casa"]
   },
   {
     id: 9,
     title: "Diávola",
     ingredients: "Salsa de tomate, queso, pepperoni, chorizo, jalapeño y pimiento",
     imageUrl:  "/images/menu/diavola.webp",
-    prices: {sm: 79, md: 189, lg: 229},
-    tags: ["picante", "carnes", "especial"]
+    prices: {sm: 89, md: 189, lg: 259},
+    tags: ["picante", "carnes", "de-la-casa"]
   },
   {
     id: 10,
     title: "Exótica",
     ingredients: "Salsa de tomate, queso, jamón, salami, aceitunas negras, pimiento y cebolla",
     imageUrl: "/images/menu/exotica.webp",
-    prices: {sm: 79, md: 189, lg: 229},
-    tags: ["carnes", "especial"]
+    prices: {sm: 89, md: 189, lg: 259},
+    tags: ["carnes", "de-la-casa"]
   },
   {
     id: 11,
     title: "Frijoleña",
     ingredients: "Frijoles refritos, queso, chorizo, tocino y jalapeño",
     imageUrl: "/images/menu/frijolena.webp",
-    prices: {sm: 79, md: 189, lg: 229},
-    tags: ["frijoles", "carnes", "especial"]
+    prices: {sm: 89, md: 189, lg: 259},
+    tags: ["frijoles", "carnes", "de-la-casa"]
   },
   {
     id: 12,
-    title: "Carroñera",
-    ingredients: "Salsa de tomate, queso, pepperoni, salami, jamón, tocino, chorizo y salchicha",
-    imageUrl: "/images/menu/carronera.webp",
-    prices: {sm: 89, md: 199, lg: 249},
-    tags: ["carnes", "especial", "mas-pedida"]
-  },
-  {
-    id: 13,
     title: "Wera",
     ingredients: "Frijoles refritos, queso, chicharrón de cerdo, chorizo, chile güero, jalapeño y cebolla",
     imageUrl: "/images/menu/wera.webp",
-    prices: {sm: 89, md: 199, lg: 249},
-    tags: ["frijoles", "carnes", "especial", "picante"]
+    prices: {sm: 89, md: 189, lg: 259},
+    tags: ["frijoles", "carnes", "de-la-casa", "picante"]
+  },
+  {
+    id: 13,
+    title: "Carroñera",
+    ingredients: "Salsa de tomate, queso, pepperoni, salami, jamón, tocino, chorizo y salchicha",
+    imageUrl: "/images/menu/carronera.webp",
+    prices: {sm: 99, md: 209, lg: 299},
+    tags: ["carnes", "especial", "mas-pedida"]
   },
   {
     id: 14,
     title: "Boneless Búfalo",
     ingredients: "Salsa de tomate, queso, boneless búfalo, apio y aderezo ranch",
     imageUrl: "/images/menu/bufalo.webp",
-    prices: {sm: 89, md: 199, lg: 249},
+    prices: {sm: 99, md: 209, lg: 299},
     tags: ["carnes", "especial", "mas-pedida", "picante"]
   },
   {
@@ -116,7 +116,7 @@ export const pizzasMenu = [
     title: "Boneless BBQ",
     ingredients: "Salsa de tomate, queso, boneless BBQ, champiñones y cebolla",
     imageUrl: "/images/menu/bbq.webp",
-    prices: {sm: 89, md: 199, lg: 249},
+    prices: {sm: 99, md: 209, lg: 299},
     tags: ["carnes", "especial", "mas-pedida"]
   },
   {
@@ -124,39 +124,47 @@ export const pizzasMenu = [
     title: "Pizza-Dogo",
     ingredients: "Salsa de tomate, queso, salchicha, salami, tocino, cebolla, champiñones y aderezo ranch",
     imageUrl: "/images/menu/dogo.webp",
-    prices: {sm: 89, md: 199, lg: 249},
+    prices: {sm: 99, md: 209, lg: 299},
     tags: ["carnes", "especial", "mas-pedida"]
+  },
+  {
+    id: 17,
+    title: "Oseana",
+    ingredients: "Salsa de tomate, queso, pepperoni, salchicha, salami, tocino, piña, champiñones y aderezo ranch",
+    imageUrl: "/images/menu/dogo.webp",
+    prices: {sm: 99, md: 209, lg: 299},
+    tags: ["carnes", "especial"]
   }
 ];
 
 export const featuredPizzas = [
+    {
+    id: "pepperoni",
+    title: "Pepperoni",
+    description: "Salsa de tomate, queso y pepperoni.",
+    imageUrl: "/images/menu/pepperoni.webp",
+    price: "$219",
+  },
+    {
+    id: "hawaiana",
+    title: "Hawaiana",
+    description: "Salsa de tomate, queso, jamón y piña.",
+    imageUrl: "/images/menu/hawaiana.webp",
+    price: "$239",
+  },
   {
     id: "margarita",
     title: "Margarita",
     description: "Salsa de tomate, queso, albahaca fresca y queso parmesano.",
     imageUrl: "/images/menu/margarita.webp",
-    price: "$159",
+    price: "$259",
   },
   {
-    id: "pepperoni",
-    title: "Pepperoni",
-    description: "Salsa de tomate, queso y pepperoni.",
-    imageUrl: "/images/menu/pepperoni.webp",
-    price: "$179",
-  },
-  {
-    id: "hawaiana",
-    title: "Hawaiana",
-    description: "Salsa de tomate, queso, jamón y piña.",
-    imageUrl: "/images/menu/hawaiana.webp",
-    price: "$169",
-  },
-  {
-    id: "mexicana",
-    title: "Mexicana",
-    description: "Salsa de tomate, queso, chorizo, jitomate, cebolla, pimiento y jalapeño.",
-    imageUrl: "/images/menu/mexicana.webp",
-    price: "$169",
+    id: "carronera",
+    title: "Carroñera",
+    description: "Salsa de tomate, queso, pepperoni, salami, jamón, tocino, chorizo y salchicha.",
+    imageUrl: "/images/menu/carronera.webp",
+    price: "$299",
   },
 ];
 
