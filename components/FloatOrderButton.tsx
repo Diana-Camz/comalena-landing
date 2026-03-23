a8297a44168a8015e1b01c13333a28dcfa5e1b74
Diana-Camz <dcampos0495@gmail.com>
2026-03-22 21:17:14 -0600

    feat: add button animation and add the preview of quantity of pizzas

--- components/FloatOrderButton.tsx
+++ components/FloatOrderButton.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useCart } from "@/context/CartContext";
 
 type ButtonProps = {
     active: boolean;
@@ -6,8 +7,10 @@ type ButtonProps = {
 }
 
 export default function FloatOrderButton({active, setActive}: ButtonProps) {
+    const {order} = useCart();
     return (
-        <button onClick={() => setActive(!active)} className="border-2 rounded-lg border-card-foreground/30 hover:scale-105 transition-transform cursor-pointer mb-2 bg-orange-100">
+        <div>
+            <button onClick={() => setActive(!active)} className="border-2 rounded-lg border-card-foreground/30 hover:scale-105 transition-transform cursor-pointer mb-2 active:scale-95 duration-120 bg-orange-100">
             {active ? (
                 <a>
                     <Image src="/images/pizza-box.png" alt="Close" width={56} height={56}/>
@@ -18,5 +21,12 @@ export default function FloatOrderButton({active, setActive}: ButtonProps) {
                 </a>
             )}
         </button>
+        {order.length > 0 && (
+            <div className="absolute -top-1 -right-1 bg-red/95 text-white rounded-full w-6 h-6 flex items-center justify-center text-sm font-normal">
+                <p>{order.reduce((total, item) => total + item.quantity, 0)}</p>
+            </div>
+        )}
+        </div>
+
     )
 }
