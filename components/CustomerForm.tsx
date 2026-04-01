c13faccca9f4624938659ab1112bef178b8c23e7
Diana-Camz <dcampos0495@gmail.com>
2026-04-01 13:29:18 -0600

    feat: create CustomerForm component

---
+++ components/CustomerForm.tsx
@@ -0,0 +1,75 @@
+import { useCart } from "@/context/CartContext"
+import { useState } from "react";
+import { ImCheckboxChecked, ImCheckboxUnchecked } from "react-icons/im";
+
+
+export default function CustomerForm () {
+    const [isCheckedPrivacy, setIsCheckedPrivacy] = useState(false);
+    const {customerInfo, setCustomerField} = useCart();
+    return (
+        <div>
+            <h2 className="text-xl text-center font-gothic text-card-foreground mb-4">¡Casi listo! Solo necesitamos algunos datos para tu orden.</h2>
+            <div>
+                <div className="flex gap-4">
+                    <input
+                    type="text"
+                    name="name"
+                    placeholder="Nombre"
+                    className="w-full mb-3 px-4 py-2 rounded-lg border-2 border-card-foreground/20 focus:border-red focus:ring-0 transition"
+                    onChange={(e) => setCustomerField("name", e.target.value)}
+                    value={customerInfo.name}
+                    />
+                    <input
+                    type="number"
+                    name="phone"
+                    placeholder="Teléfono"
+                    className="w-full mb-3 px-4 py-2 rounded-lg border-2 border-card-foreground/20 focus:border-red focus:ring-0 transition"
+                    onChange={(e) => setCustomerField("phone", e.target.value)}
+                    value={customerInfo.phone}
+                    />
+                </div>
+                <input
+                    type="text"
+                    name="address"
+                    placeholder="Dirección"
+                    className="w-full mb-1 px-4 py-2 rounded-lg border-2 border-card-foreground/20 focus:border-red focus:ring-0 transition"
+                    onChange={(e) => setCustomerField("address", e.target.value)}
+                    value={customerInfo.address}
+                />
+                <button
+                 className="w-full text-red mb-3 flex items-center gap-1 ml-2 cursor-pointer"
+                 onClick={() => { 
+                    const nextValue = !customerInfo.isPickup;
+                    setCustomerField("isPickup", nextValue);
+                    setCustomerField("address", nextValue ? "Pasare a recoger mi orden" : "");}}>
+                   {customerInfo.isPickup 
+                    ? <ImCheckboxChecked /> 
+                    : <ImCheckboxUnchecked />}
+                     <p className="text-md font-gothic text-card-foreground/80">No necesito agregar domicilio, pasaré a recoger mi orden</p>
+                </button>
+                <textarea
+                    name="notes"
+                    placeholder="Observaciones (opcional)"
+                    className="w-full mb-3 px-4 py-2 rounded-lg border-2 border-card-foreground/20 focus:border-red focus:ring-0 transition"
+                    onChange={(e) => setCustomerField("notes", e.target.value)}
+                    value={customerInfo.notes}
+                />
+                <button
+                 className="w-full text-red mb-3 flex items-center gap-1 ml-2 cursor-pointer"
+                 onClick={() => {
+                    setIsCheckedPrivacy(!isCheckedPrivacy); 
+                    }}>
+                   {isCheckedPrivacy
+                    ? <ImCheckboxChecked /> 
+                    : <ImCheckboxUnchecked />}
+                     <p className="text-md font-gothic text-card-foreground/80">Acepto el aviso de privacidad</p>
+                </button>
+                <div>
+                    <p className="text-lg font-gothic text-center text-red/80 leading-tight">
+                        Recuerda que al recibir tu orden, te indicaremos el tiempo de espera que tomara hacer tu pedido y el costo del servicio a domicilio (si aplica).
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
