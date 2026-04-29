19236850a1f8d23d9c63dbf16efe1ab7edb6caf9
Diana-Camz <dcampos0495@gmail.com>
2026-04-28 18:42:58 -0600

    update view

--- app/failed/page.tsx
+++ app/failed/page.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+"use client"
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
 
  function Failed() {
+  const router = useRouter()
   return (
-    <div>Failed</div>
+      <section className="min-h-[60vh] flex flex-col text-center items-center justify-center gap-6 px-4">
+        <div className='flex items-center justify-center'>
+          <p className='text-5xl md:text-7xl lg:text-9xl'>⚠️</p>
+        </div>
+        <h2 className="text-[clamp(1.5rem,2.5vw,2.5rem)] text-black/80">Tu pago no se realizó</h2>
+        <div className='flex justify-center items-center '>
+          <Button
+          type="button" 
+          onClick={() => {router.push('/menu')}}
+          className="cursor-pointer h-12 bg-red/95 hover:bg-red/80 active:bg-red/80 active:scale-97 transition duration-120 text-background text-lg font-medium">
+          <p className="text-md md:text-lg">Regresar al menú</p>
+        </Button>
+        </div>
+      </section>
   )
 }
 
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+"use client"
+import { Button } from '@/components/ui/button';
+import { PiSealCheckDuotone } from "react-icons/pi";
+import { useCart } from '@/context/CartContext'
+import { useRouter } from 'next/navigation';
+
 
 function Success() {
+  const { buildWhatsAppMessage, clearCart } = useCart();
+  const router = useRouter();
+
+  const handleSendWhatsApp = () => {
+    const message = buildWhatsAppMessage();
+    if (!message) return;
+    window.open(message, "_blank");
+    clearCart()
+    router.push("/menu")
+  }
   return (
-    <div>Success</div>
+      <section className="min-h-[60vh] flex flex-col text-center items-center justify-center gap-6 px-4">
+        <div className='flex items-center justify-center'>
+          <PiSealCheckDuotone className='w-20 h-20 md:w-30 md:h-30 lg:w-50 lg:h-50 text-[#3cb15f]'/>
+        </div>
+        <h2 className="text-[clamp(1.5rem,2.5vw,2.5rem)] text-black/80 ">Pago realizado correctamente</h2>
+        <div className='flex justify-center items-center '>
+          <Button
+          type="button" 
+          onClick={handleSendWhatsApp}
+          className="cursor-pointer h-12 bg-red/95 hover:bg-red/80 active:bg-red/80 active:scale-97 transition duration-120 text-background text-lg font-medium">
+          <p className="text-md md:text-lg">Enviar mi orden y mi comprobante de pago</p>
+        </Button>
+        </div>
+      </section>
   )
 }
 
